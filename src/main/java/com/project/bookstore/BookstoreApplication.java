e33b0b7a8a1a4cac08076f2d65ff425b791a3914
singh-rbir <43500177+singh-rbir@users.noreply.github.com>
2021-02-27 15:07:36 -0500

    Add dummy cron job to keep server from hibernation

--- src/main/java/com/project/bookstore/BookstoreApplication.java
+++ src/main/java/com/project/bookstore/BookstoreApplication.java
@@ -3,6 +3,7 @@ package com.project.bookstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -13,6 +14,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 //@EnableJpaRepositories(basePackages = "com.project.bookstore.dao")
 @SpringBootApplication
 @EnableWebMvc
+@EnableScheduling
 public class BookstoreApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/project/bookstore/controller/MainController.java
+++ src/main/java/com/project/bookstore/controller/MainController.java
@@ -1,5 +1,8 @@
 package com.project.bookstore.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +10,8 @@ import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class MainController {
+
+  Logger log = LoggerFactory.getLogger(MainController.class);
 
   @RequestMapping("/")
   public String home(){
@@ -18,9 +23,14 @@ public class MainController {
     return "Hello. This is 4413 project.\nProject members are:\n1. Rushil\n2. Aman\n3. Rajanbir";
   }
 
-//  @GetMapping(value = "/error")
-//  public ModelAndView error(){
-//    return new ModelAndView("error404");
-//  }
+  @GetMapping(value = "/error")
+  public ModelAndView error(){
+    return new ModelAndView("error404");
+  }
+
+  @Scheduled(cron = "0 */30 * * * *")
+  public void dummyWorkerJob(){
+    log.error("cron job ran");
+  }
 
 }
